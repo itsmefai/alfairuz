9806512d27f705d43d455e92bb3113ab8e162550
itsmefai <52740390+itsmefai@users.noreply.github.com>
2020-11-08 13:26:42 +0800

    added folder

--- Data Analysis/Titanic/src/titanic_analysis.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# import libraries for analysis
-import pandas as pd
-import seaborn as sns
-
-df = pd.read_csv('data/train.csv')
-
-
-#step 1: identify dependent and independent variable
